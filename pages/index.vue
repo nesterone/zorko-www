7f3a56dc46b733814ffe366ee3b3df2839994f7e
nesterone <igornester@gmail.com>
2017-10-30 14:10:23 +0200

    - upgrade nuxt to 1.0.0 - introduce vuetify

--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,30 @@
 <template>
-    <div id="app">
-        <div class="js-Landing">
-            <img src="~assets/vg.png">
-            <h1>{{ msg }}</h1>
-            <a class="button--default" :href="zorkoVgcodeUrl">Try It!</a>
-
-            <h2>Essential Link !!!!!</h2>
-            <ul>
-                <li><a href="http://example.com" target="_blank">Twitter</a></li>
-                <li><a href="http://example.com" target="_blank">Discuss</a></li>
-                <li><a href="http://example.com" target="_blank">Slack</a></li>
-                <li><a href="http://example.com" target="_blank">Issues</a></li>
-            </ul>
-        </div>
-    </div>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-xs-center">
+        <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      </div>
+      <v-card>
+        <v-card-title class="headline">Welcome to the Vuetify ++ Nuxt.js template</v-card-title>
+        <v-card-text>
+          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
+          <p>For more information on Vuetify, check out the <a href="https://vuetifyjs.com" target="_blank">documentation</a>.</p>
+          <p>If you have questions, please join the official <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">discord</a>.</p>
+          <p>Find a bug? Report it on the github <a href="https://github.com/vuetifyjs/vuetify/issues" target="_blank" title="contribute">issue board</a>.</p>
+          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
+          <div class="text-xs-right">
+            <em><small>&mdash; John Leider</small></em>
+          </div>
+          <hr class="my-3">
+          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
+          <br>
+          <a href="https://github.com/nuxt/nuxt.js" target="_blank">Nuxt GitHub</a>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" flat nuxt to="/inspire">Continue</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
-
-<script>
-  export default {
-    name: 'landing',
-
-    data () {
-      return {
-        msg: 'Welcome to Vega Code !!!'
-      }
-    },
-
-    asyncData ({ env }) {
-      return {
-        zorkoVgcodeUrl: env.zorkoVgcodeUrl
-      }
-    }
-  }
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
-
-    .button--default {
-        font-size: 1.2em;
-        padding: 0.75em 2em;
-        border-radius: 2em;
-        display: inline-block;
-        color: #fff;
-        background-color: #4fc08d;
-        transition: all 0.15s ease;
-        box-sizing: border-box;
-        border: 1px solid #4fc08d;
-    }
-
-    h1, h2 {
-        font-weight: normal;
-    }
-
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
-
-    li a {
-        color: #42b983;
-    }
-
-</style>
